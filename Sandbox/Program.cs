cde83cf5361c964778b47679d74ca96f34b6bb0e
Aleg Matus <aleg.matus@grothetechnology.com>
2023-05-23 00:00:51 +0200

    improved array output formatting added solution for TwoSum

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Sandbox.TaskSolving;
 using Sandbox.TaskSolving.TaskFunctions;
-using Sandbox.TestDataGeneration;
 using Sandbox.TestDataGeneration.RandomValueFactories;
+using System.Text;
 
 #region Value factories
 RandomIntegerFactory randomIntegerFactory = new RandomIntegerFactory();
@@ -17,8 +17,47 @@ int parameterSets = 2;
 
 for (int i = 0; i < testCount; i++)
 {
-    var firstString = TestData.GetTestData(randomStringFactory);
-    var secondString = TestData.GetTestData(randomStringFactory);
-    var result = TaskSolver.SolveTask(ValidAnagram.IsAnagram, firstString, secondString);
-    Console.WriteLine($"#{i + 1}: Parameters: [{string.Join(", ", firstString, secondString)}]\n\tResult: {result}");
+    var nums = new int[] { 3, 2, 4 };
+    var target = 6;
+    var result = TaskSolver.SolveTask(TwoSum.Solve, nums, target);
+    Console.WriteLine($"#{i + 1}: Parameters: {FormatArray(nums, target)}\n\tResult: {FormatArray(result)}");
 }
+
+#region Output formatting
+string FormatArray(params object[] values)
+{
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        if (values[i] is Array array)
+        {
+            builder.Append("[");
+            builder.Append(string.Join(", ", GetArrayValues(array)));
+            builder.Append("]");
+        }
+        else
+        {
+            builder.Append(values[i]);
+        }
+
+        if (i < values.Length - 1)
+        {
+            builder.Append(", ");
+        }
+    }
+
+    return builder.ToString();
+}
+
+string[] GetArrayValues(Array array)
+{
+    string[] stringValues = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        stringValues[i] = array.GetValue(i)?.ToString()!;
+    }
+
+    return stringValues;
+}
+#endregion
---
+++ Sandbox/TaskSolving/TaskFunctions/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox.TaskSolving.TaskFunctions;
+
+public static class TwoSum
+{
+    public static int[] Solve(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
