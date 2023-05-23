6c6eb3ec1aa0590ae51f73dd80251ca4bfa8796a
Aleg Matus <aleg.matus@grothetechnology.com>
2023-05-23 22:06:58 +0200

    added solution for GroupAnagrams

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Sandbox.TaskSolving;
 using Sandbox.TaskSolving.TaskFunctions;
 using Sandbox.TestDataGeneration.RandomValueFactories;
+using System.Collections;
 using System.Text;
 
 #region Value factories
@@ -11,29 +12,46 @@ RandomDoubleFactory randomDoubleFactory = new RandomDoubleFactory();
 RandomCharFactory randomCharFactory = new RandomCharFactory();
 #endregion
 
-int testCount = 3;
+int testCount = 1;
 int parameterSets = 2;
 
 
 for (int i = 0; i < testCount; i++)
 {
-    var nums = new int[] { 3, 2, 4 };
+    var strings = new string[] { "", "" };
     var target = 6;
-    var result = TaskSolver.SolveTask(TwoSum.Solve, nums, target);
-    Console.WriteLine($"#{i + 1}: Parameters: {FormatArray(nums, target)}\n\tResult: {FormatArray(result)}");
+    var result = TaskSolver.SolveTask(GroupAnagrams.Solve, strings);
+    Console.WriteLine($"#{i + 1}: Parameters: {FormatMultipleValues(strings, target)}\n\tResult: {FormatMultipleValues(result)}");
 }
 
 #region Output formatting
-string FormatArray(params object[] values)
+string FormatMultipleValues(params object[] values)
 {
     StringBuilder builder = new StringBuilder();
 
     for (int i = 0; i < values.Length; i++)
     {
-        if (values[i] is Array array)
+
+        if (values[i] is IList<IList<string>> nestedList)
         {
             builder.Append("[");
-            builder.Append(string.Join(", ", GetArrayValues(array)));
+            for (int j = 0; j < nestedList.Count; j++)
+            {
+                builder.Append("[");
+                builder.Append(string.Join(", ", nestedList[j]));
+                builder.Append("]");
+
+                if (j < nestedList.Count - 1)
+                {
+                    builder.Append(", ");
+                }
+            }
+            builder.Append("]");
+        }
+        else if (values[i] is IList<string> list)
+        {
+            builder.Append("[");
+            builder.Append(string.Join(", ", list));
             builder.Append("]");
         }
         else
@@ -50,14 +68,26 @@ string FormatArray(params object[] values)
     return builder.ToString();
 }
 
-string[] GetArrayValues(Array array)
-{
-    string[] stringValues = new string[array.Length];
-    for (int i = 0; i < array.Length; i++)
-    {
-        stringValues[i] = array.GetValue(i)?.ToString()!;
-    }
+//string[] GetArrayValues(Array array)
+//{
+//    string[] stringValues = new string[array.Length];
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        stringValues[i] = array.GetValue(i)?.ToString()!;
+//    }
 
-    return stringValues;
-}
+//    return stringValues;
+//}
+
+//string[] GetEnumerableValues(IEnumerable enumerable)
+//{
+//    List<string> stringValues = new List<string>();
+
+//    foreach (var item in enumerable)
+//    {
+//        stringValues.Add(item?.ToString() ?? string.Empty);
+//    }
+
+//    return stringValues.ToArray();
+//}
 #endregion
---
+++ Sandbox/TaskSolving/TaskFunctions/GroupAnagrams.cs
@@ -0,0 +1,72 @@
+﻿namespace Sandbox.TaskSolving.TaskFunctions;
+
+public static class GroupAnagrams
+{
+    public static IList<IList<string>> Solve(string[] strs)
+    {
+        var result = new List<IList<string>>();
+        for (int i = 0; i < strs.Length; i++)
+        {
+            var tempList = new List<string>();
+            if (!tempList.Contains(strs[i]) && !Contains(result, strs[i]))
+            {
+                tempList.Add(strs[i]);
+                for (int j = i + 1; j + 1 <= strs.Length; j++)
+                {
+                    if (IsAnagram(strs[i], strs[j]))
+                    {
+                        tempList.Add(strs[j]);
+                    }
+                }
+                
+                result.Add(tempList);
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsAnagram(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t))
+        {
+            return true;
+        }
+
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        int[] charCount = new int[26];
+        for (int i = 0; i < s.Length; i++)
+        {
+            charCount[s[i] - 'a']++;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            charCount[t[i] - 'a']--;
+            if (charCount[t[i] - 'a'] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool Contains(List<IList<string>> strings, string value)
+    {
+        foreach (var item in strings)
+        {
+            foreach (var item2 in item)
+            {
+                if (item2 == value)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
